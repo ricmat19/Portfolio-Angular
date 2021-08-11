978f2ec7b4f196ece0595dc5c341218be4316856
Ricky Del Cueto <ric19arthouse@gmail.com>
2021-08-10 23:48:37 -0400

    File Rename & Thumbnail Grid Setup

---
+++ portfolio/frontend/src/components/portfolio.jsx
@@ -0,0 +1,86 @@
+import React, { useContext, useState } from 'react';
+import HeaderC from './header';
+import FooterC from './footer';
+import {PortfolioContext} from '../context/portfolio';
+import {useHistory} from "react-router-dom";
+
+
+const PortfolioC = () => {
+
+    const {portfolio, setPortfolio} = useContext(PortfolioContext);
+    const [pageNumber, setPageNumber] = useState(0);
+
+    let history = useHistory();
+
+    const itemsPerPage = 9;
+    const pagesVisted = pageNumber * itemsPerPage;
+
+    // const portfolioThumbnails = portfolio.slice(pagesVisted, pagesVisted + itemsPerPage).map((project) => {
+    //     return(
+    //         <div className="portfolio-item-div" key={project.id} onClick={() => displayProject(project.product, project.id)}>
+    //             <div className="portfolio-project">
+    //                 <img className="project-thumbnail" src={project.imageBuffer}/>
+    //             </div>
+    //         </div>
+    //     );
+    // });
+
+    const displayProject = async (product, id) => {
+        try{
+            history.push(`/collection/${product}/${id}`)
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+    return(
+        <div className="">
+            <HeaderC/>
+            <div className="container">
+                <div className="title-div">
+                    <p className="title">portfolio</p>
+                </div>
+                <div className="portfolio-thumbnail-div">
+                    <div className="portfolio-item-div">
+                        <div className="portfolio-project">
+                            <img className="project-thumbnail" src="../../images/smiles.jpg"/>
+                        </div>
+                    </div>
+                    <div className="portfolio-item-div">
+                        <div className="portfolio-project">
+                            <img className="project-thumbnail" src="../../images/smiles.jpg"/>
+                        </div>
+                    </div>
+                    <div className="portfolio-item-div">
+                        <div className="portfolio-project">
+                            <img className="project-thumbnail" src="../../images/smiles.jpg"/>
+                        </div>
+                    </div>
+                    <div className="portfolio-item-div">
+                        <div className="portfolio-project">
+                            <img className="project-thumbnail" src="../../images/smiles.jpg"/>
+                        </div>
+                    </div>
+                    <div className="portfolio-item-div">
+                        <div className="portfolio-project">
+                            <img className="project-thumbnail" src="../../images/smiles.jpg"/>
+                        </div>
+                    </div>
+                    <div className="portfolio-item-div">
+                        <div className="portfolio-project">
+                            <img className="project-thumbnail" src="../../images/smiles.jpg"/>
+                        </div>
+                    </div>
+                    <div className="portfolio-item-div">
+                        <div className="portfolio-project">
+                            <img className="project-thumbnail" src="../../images/smiles.jpg"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <FooterC/>
+        </div>
+    )
+}
+
+export default PortfolioC;
